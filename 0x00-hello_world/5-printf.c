1712452dd325cdad9a42aed451a9f48ee9547166
MostafaMohamed142 <simbawyy10@gmail.com>
2022-10-28 09:28:17 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * entry point
+ *
+ * return always 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main entry point 
+ *
+ * return always 0 (sucess)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("size of int: %lu bytes\n" ,(unsigned long)sizeof(a));
+	printf("size of long int: %lu bytes\n",(unsigned long)sizeof(b));
+			printf("size of long long int: %lu bytes\n",(unsigned long)sizeof(c));
+				printf("size of char : %lu bytes\n",(unsigned long)sizeof(d));
+					printf("size of float: %lu bytes\n",(unsigned long)sizeof(e));
+					return(0);
+				};
